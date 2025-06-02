cdc6de2203db45b1e7ea9dc9ffaa533dacbc4d01
Revathy M <revathy.m@neutrinos.com>
2025-06-02 15:19:37 +0530

    changes updated

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output} from '@angular/core';
-
+import {User} from './user.model'
 
 @Component({
   selector: 'app-user',
@@ -8,14 +8,13 @@ import { Component, EventEmitter, Input, Output} from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
- @ Input({required :true}) id! : string;
- @ Input({required : true}) avatar! : string;
- @ Input({required : true}) name! : string ;
+ @ Input({required :true}) user! : User;
+ @ Input ({required : true}) selected!: Boolean;
  @ Output () select = new EventEmitter()
  get imagePath(){
-  return 'assets/users/'+ this.avatar
+  return 'assets/users/'+ this.user.avatar
  }
   onSelectUser() {
-    this.select.emit(this.id)
+    this.select.emit(this.user.id)
   }
 } 
